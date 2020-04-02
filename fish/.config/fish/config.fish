c2cd5ff6cf83054915f756bd9c49c2763964457a
Asa Salo <anttimeister@gmail.com>
2020-04-02 03:27:12 +0300

    Add alias for removing mouse acceleration

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -6,3 +6,4 @@ export LC_CTYPE=en_US.UTF-8
 alias vi='nvim'
 alias la='ls -lahG'
 alias conf='vi ~/.config/fish/config.fish'
+alias maus='defaults write .GlobalPreferences com.apple.mouse.scaling -1'
